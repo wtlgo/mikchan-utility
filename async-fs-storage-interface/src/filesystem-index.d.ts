efaacae3cba80c257e19cceac5db5c6c6aa52053
wtlgo <georgeendo0@gmail.com>
2024-03-22 16:50:46 +0300

    Remove build phase

--- async-fs-storage-interface/src/filesystem-index.d.ts
+++ async-fs-storage-interface/src/filesystem-index.d.ts
@@ -1,9 +1,7 @@
 import { FileMeta } from "./file-meta";
-
 export interface FileSystemIndex {
     getMeta(key: string, value?: string | undefined | null): Promise<FileMeta>;
     saveMeta(key: string, value: string): Promise<void>;
     removeMeta(key: string): Promise<void>;
-
     getAllKeys(): Promise<string[]>;
 }
